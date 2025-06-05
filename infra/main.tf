0877ca41b5686bf5f59fb58513531b044dd140dd
Барановский <nbaranovskii@astralinux.ru>
2025-06-05 20:30:46 +0300

    add parametrs to s3

--- infra/main.tf
+++ infra/main.tf
@@ -15,6 +15,8 @@ terraform {
     key                         = "tf-state.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
