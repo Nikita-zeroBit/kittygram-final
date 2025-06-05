eadd278d3b18060c54897d545b92edfe4df0c6e0
Барановский <nbaranovskii@astralinux.ru>
2025-06-05 20:41:57 +0300

    fix tf version

--- infra/main.tf
+++ infra/main.tf
@@ -7,16 +7,12 @@ terraform {
   }
 
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
+    endpoints = "https://storage.yandexcloud.net"
     bucket                      = "kitty-s3-backet"
     region                      = "ru-central1"
     key                         = "tf-state.tfstate"
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
   }
 }
 
