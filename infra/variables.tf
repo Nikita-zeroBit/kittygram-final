a76ea66df2f2ab297b61d175aaac212717838135
Барановский <nbaranovskii@astralinux.ru>
2025-06-08 19:25:02 +0300

    fix ssh keys

--- infra/variables.tf
+++ infra/variables.tf
@@ -49,7 +49,7 @@ variable "vm_image_id" {
   default     = "fd80bm0rh4rkepi5ksdi" # ubuntu-24-04-lts-v20250324
 }
 
-variable "ssh_key" {
+variable "ssh_pub_key" {
   description = "Публичной SSH-ключ"
   type        = string
 }
